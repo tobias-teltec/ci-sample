c79bf691cc0edef1f6d9d6103b12d2694990133a
tobias-teltec <tobias@tobias-teltec.localdomain>
2020-10-14 20:20:34 -0300

    instance config ident

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,11 +1,9 @@
-  1 # Configure the AWS Provider
-  2 provider "aws" {
-  3   region  = "us-east-1"
-  4     5   profile = "awsterraform"
-  6 }
-  7 
-  8 
-  9 resource "aws_instance" "modeloteste" {
- 10   ami = "ami-00a208c7cdba991ea"
- 11   instance_type = "t2.micro"
- 12 }
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"
+  profile = "awsterraform"
+}
+resource "aws_instance" "modeloteste" {
+  ami = "ami-00a208c7cdba991ea"
+  instance_type = "t2.micro"
+}
